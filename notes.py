44f91b31ccc94af64a8f294f5f6802d370eb72d3
JRileyStansell <42385707+JRileyStansell@users.noreply.github.com>
2019-01-08 09:45:56 -0800

    hello

---
+++ notes.py
@@ -0,0 +1,19 @@
+print("Hello World!")
+# What do I put here? Blah blah blah lorem ipsum dolor
+cars = 5
+driving = True
+print("I have %d cars." %cars)
+print("I have " + str(cars) + " cars.")
+print("* Concatenation.")
+
+age = input("How old are you?")
+print(age + "? That's young.")
+
+colors = ["cyan", "orange", "blue", "purple", "green", "yellow", "red"]
+colors.append("pink")
+# okay how do i remove something from a list without using list.remove
+colors.pop(0)
+
+print(colors)
+print(colors[2])
+print(len(colors))
