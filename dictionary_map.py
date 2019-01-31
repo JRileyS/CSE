f1af7bd064f9e087fbd76a3db948aff1333d3bef
JRileyS <42385707+JRileyS@users.noreply.github.com>
2019-01-31 09:45:06 -0800

    Dictionaries 2.0
    I made an experimental map based on the start of Undertale to try out making a new map.

--- dictionary_map.py
+++ dictionary_map.py
@@ -1,54 +1,55 @@
-world_map = {
-    'R19A': {
-        'NAME': "Mr. Wiebe's Room",
-        'DESCRIPTION': "This is the classroom you are in right "
-                       "now. There are two doors on the north wall.",
-        'PATHS': {
-            'NORTH': "PARKING_LOT"
-        }
-    },
-    'PARKING_LOT': {
-        'NAME': "The North Parking Lot",
-        'DESCRIPTION': "There are a couple cars parked here.",
-        'PATHS': {
-            'SOUTH': 'R19A'
-        }
-    }
-}
-
-######Jacob's Experiment######
 #world_map = {
-#    'FLOWERBED': {
-#        'NAME': "Flowerbed",
-#        'DESCRIPTION': "You're suddenly in a strange flowerbed, at the "
-#                       "bottom of a giant fall. How'd you survive that? "
-#                       "There's also a path to the east of you.",
+#    'R19A': {
+#        'NAME': "Mr. Wiebe's Room",
+#        'DESCRIPTION': "This is the classroom you are in right "
+#                       "now. There are two doors on the north wall.",
 #        'PATHS': {
-#            'EAST': "GREAT_DOOR"
+#            'NORTH': "PARKING_LOT"
 #        }
 #    },
-#    'GREAT_DOOR': {
-#        'NAME': "The Great Door",
-#        'DESCRIPTION': "All you can see is a door to the north and a "
-#                      "path to the west back to the flowerbed.",
+#    'PARKING_LOT': {
+#        'NAME': "The North Parking Lot",
+#        'DESCRIPTION': "There are a couple cars parked here.",
 #        'PATHS': {
-#            'WEST': 'FLOWERBED',
-#            'NORTH': 'FLOWEY'
-#        }
-#    },
-#    'FLOWEY': {
-#        'NAME': 'Strange Room',
-#        'DESCRIPTION': "You can see a strange flower in the room, "
-#                       "just standing there. It has a face?",
-#        'PATHS': {
-#            'SOUTH': 'GREAT_DOOR'
+#            'SOUTH': 'R19A'
 #        }
 #    }
 #}
 
+######Jacob's Experiment######
+world_map = {
+    'FLOWERBED': {
+        'NAME': "Flowerbed",
+        'DESCRIPTION': "You're suddenly in a strange flowerbed, at the "
+                       "bottom of a giant fall. How'd you survive that? "
+                       "There's also a path to the east of you.",
+        'PATHS': {
+            'EAST': "GREAT_DOOR_1"
+        }
+    },
+    'GREAT_DOOR_1': {
+        'NAME': "The Great Door",
+        'DESCRIPTION': "All you can see is a door to the north and a "
+                      "path to the west back to the flowerbed.",
+        'PATHS': {
+            'WEST': 'FLOWERBED',
+            'NORTH': 'FLOWEY'
+        }
+    },
+    'FLOWEY': {
+        'NAME': 'Strange Room',
+        'DESCRIPTION': "You can see a strange flower in the room, "
+                       "just standing there. The flower has "
+                       "a... face?!",
+        'PATHS': {
+            'SOUTH': 'GREAT_DOOR_1'
+        }
+    }
+}
+
 # Controller
 playing = True
-current_node = world_map['R19A']
+current_node = world_map['FLOWERBED']
 directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'UP', 'DOWN']
 
 while playing:
@@ -57,8 +58,11 @@ while playing:
     command = input("> ")
     if command.lower() in ['q', 'quit', 'exit']:
         playing = False
-    elif command in directions:
-        room_name = current_node['PATHS'][command]
-        current_node = world_map[room_name]
+    elif command.upper() in directions:
+        try:
+            room_name = current_node['PATHS'][command.upper()]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("You can't go that way.")
     else:
         print("I don't know that command.")
