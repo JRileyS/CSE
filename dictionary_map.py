328e9d6ac8321674a5db42f8ccda4ecafc081863
JRileyS <42385707+JRileyS@users.noreply.github.com>
2019-01-31 12:51:02 -0800

    Dictionaries 3.0
    Decided to delete the base map from the map for the open-world game, replacing it with the experiment map I made.

--- dictionary_map.py
+++ dictionary_map.py
@@ -1,21 +1,3 @@
-#world_map = {
-#    'R19A': {
-#        'NAME': "Mr. Wiebe's Room",
-#        'DESCRIPTION': "This is the classroom you are in right "
-#                       "now. There are two doors on the north wall.",
-#        'PATHS': {
-#            'NORTH': "PARKING_LOT"
-#        }
-#    },
-#    'PARKING_LOT': {
-#        'NAME': "The North Parking Lot",
-#        'DESCRIPTION': "There are a couple cars parked here.",
-#        'PATHS': {
-#            'SOUTH': 'R19A'
-#        }
-#    }
-#}
-
 ######Jacob's Experiment######
 world_map = {
     'FLOWERBED': {
