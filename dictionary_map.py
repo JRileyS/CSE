19cf5f37e3d939c871502a701ae8846ef5cb94f6
JRileyS <42385707+JRileyS@users.noreply.github.com>
2019-02-01 10:39:47 -0800

    Dogs represent the coders.
    You see a dog in the Eastern Arena, coding a game. Maybe you should leave it be.

--- dictionary_map.py
+++ dictionary_map.py
@@ -1,55 +1,44 @@
-#world_map = {
-#    'R19A': {
-#        'NAME': "Mr. Wiebe's Room",
-#        'DESCRIPTION': "This is the classroom you are in right "
-#                       "now. There are two doors on the north wall.",
-#        'PATHS': {
-#            'NORTH': "PARKING_LOT"
-#        }
-#    },
-#    'PARKING_LOT': {
-#        'NAME': "The North Parking Lot",
-#        'DESCRIPTION': "There are a couple cars parked here.",
-#        'PATHS': {
-#            'SOUTH': 'R19A'
-#        }
-#    }
-#}
-
 ######Jacob's Experiment######
 world_map = {
-    'FLOWERBED': {
-        'NAME': "Flowerbed",
-        'DESCRIPTION': "You're suddenly in a strange flowerbed, at the "
+    'START_ROOM': {
+        'NAME': "Starting Room",
+        'DESCRIPTION': "You're suddenly in a strange room, at the "
                        "bottom of a giant fall. How'd you survive that? "
-                       "There's also a path to the east of you.",
+                       "There's paths to each cardinal direction.",
         'PATHS': {
-            'EAST': "GREAT_DOOR_1"
+            'EAST': "EAST_START",
+            'WEST': "WEST_START",
+            'NORTH': "NORTH_START",
+            'SOUTH': "SOUTH_START"
         }
     },
-    'GREAT_DOOR_1': {
-        'NAME': "The Great Door",
-        'DESCRIPTION': "All you can see is a door to the north and a "
-                      "path to the west back to the flowerbed.",
+    'EAST_START': {
+        'NAME': "Eastern Start",
+        'DESCRIPTION': "You can see paths to the east and north, "
+                       "along with a path west, back to where you started.",
         'PATHS': {
-            'WEST': 'FLOWERBED',
-            'NORTH': 'FLOWEY'
+            'EAST': 'EAST_ARENA',
+            'NORTH': 'ARMORY'
         }
     },
-    'FLOWEY': {
-        'NAME': 'Strange Room',
-        'DESCRIPTION': "You can see a strange flower in the room, "
-                       "just standing there. The flower has "
-                       "a... face?!",
+    'WEST_START': {
+        'NAME': "Western Start",
+        'DESCRIPTION': "You can see a path to your west, along with "
+                       "a path to the east back to your starting point."
+    },
+    'EAST_ARENA': {
+        'NAME': "Eastern Arena",
+        'DESCRIPTION': "You see a dog in the arena, coding a "
+                       "game. Maybe you should leave it be.",
         'PATHS': {
-            'SOUTH': 'GREAT_DOOR_1'
+            'WEST': 'EAST_START'
         }
     }
 }
 
 # Controller
 playing = True
-current_node = world_map['FLOWERBED']
+current_node = world_map['START_ROOM']
 directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'UP', 'DOWN']
 
 while playing:
