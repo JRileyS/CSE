3fa71b84c7e6fbb780bad2b3d73b40ee0e923a03
JRileyS <42385707+JRileyS@users.noreply.github.com>
2019-02-05 09:27:21 -0800

    Island of the Blue Dolphins
    I rewrote my story to where you wake up in the middle of an island overrun by dogs, instead of an abandoned collesium. I also added a couple new rooms to the code.

--- dictionary_map.py
+++ dictionary_map.py
@@ -2,8 +2,8 @@
 world_map = {
     'START_ROOM': {
         'NAME': "Starting Room",
-        'DESCRIPTION': "You're suddenly in a strange room, at the "
-                       "bottom of a giant fall. How'd you survive that? "
+        'DESCRIPTION': "You're suddenly in a strange forest, in the "
+                       "middle of an island. How'd you get here? "
                        "There's paths to each cardinal direction.",
         'PATHS': {
             'EAST': "EAST_START",
@@ -17,24 +17,42 @@ world_map = {
         'DESCRIPTION': "You can see paths to the east and north, "
                        "along with a path west, back to where you started.",
         'PATHS': {
-            'EAST': 'EAST_ARENA',
-            'NORTH': 'ARMORY'
+            'EAST': 'EAST_DOG',
+            'NORTH': 'ARMORY',
+            'WEST': 'START_ROOM'
         }
     },
     'WEST_START': {
         'NAME': "Western Start",
         'DESCRIPTION': "You can see a path to your west, along with "
-                       "a path to the east back to your starting point."
+                       "a path to the east back to your starting point.",
+        'PATHS': {
+            'WEST': 'WEST_DOG',
+            'EAST': 'START_ROOM'
+        }
     },
-    'EAST_ARENA': {
-        'NAME': "Eastern Arena",
-        'DESCRIPTION': "You see a dog in the arena, coding a "
-                       "game. Maybe you should leave it be.",
+    'EAST_DOG': {
+        'NAME': "Eastern Dog Room",
+        'DESCRIPTION': "You see a dog in this area, coding a "
+                       "game. Maybe you should leave it be. "
+                       "Or, maybe you can befriend it?",
         'PATHS': {
             'WEST': 'EAST_START'
         }
+    },
+    'ARMORY': {
+        'NAME': "Armory",
+        'DESCRIPTION': "You find some abandoned armor here. Maybe "
+                       "another person came here before you? "
+                       "There's also a path south and a path west.",
+        'PATHS': {
+            'WEST': 'NORTH_START',
+            'SOUTH': 'EAST_START'
+        }
     }
 }
+
+
 
 # Controller
 playing = True
